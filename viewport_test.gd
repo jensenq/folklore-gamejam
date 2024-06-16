9f68b4b66f2aec8ac14a3bbed3d9e5848d718722
jensenq <jensenq@wwu.edu>
2024-06-16 13:52:16 -0700

    splitscreen complete

---
+++ viewport_test.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+#
+#@onready var players := {
+	#"1": {
+		#viewport = $SubViewportContainer/SubViewport,
+		#camera = $SubViewportContainer/SubViewport/Camera3D,
+		#player = $SubViewportContainer/SubViewport/main/kraken
+	#},
+	#"2": {
+		#viewport = $SubViewportContainer2/SubViewport,
+		#camera = $SubViewportContainer2/SubViewport/Camera3D,
+		#player = $SubViewportContainer/SubViewport/main/ship
+	#}
+	#
+	#
+#}
