774ae656adc56218deb0015b3321ca8d2d276295
jensenq <jensenq@wwu.edu>
2024-06-16 15:10:57 -0700

    collision started

--- kraken.gd
+++ kraken.gd
@@ -1,12 +1,30 @@
 extends CharacterBody3D
 
-# How fast the player moves in meters per second.
+signal hit
 @export var speed = 14
-
 var target_velocity = Vector3.ZERO
+
+func detect_collision():
+	
+	# Iterate through all collisions that occurred this frame
+	for index in range(get_slide_collision_count()):
+		# We get one of the collisions with the player
+		var collision = get_slide_collision(index)
+
+		# If the collision is with ground
+		if collision.get_collider() == null:
+			continue
+
+		# If the collider is with a mob
+		if collision.get_collider().is_in_group("ship"):
+			var ship = collision.get_collider()
+			#print("ship and kraken collided")
 
 
 func _physics_process(delta):
+	
+	detect_collision()
+	
 	var direction = Vector3.ZERO
 
 	if Input.is_action_pressed("p1_right"):
@@ -29,3 +47,11 @@ func _physics_process(delta):
 	# Moving the Character
 	velocity = target_velocity
 	move_and_slide()
+
+func die():
+	hit.emit()
+	queue_free()
+
+func _on_hit_detector_body_entered(body):
+	die()
+	print("ship hit")
